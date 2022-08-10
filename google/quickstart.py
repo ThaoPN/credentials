236dea60f2ebb5869a796e707dc43e2ea429b70d
Thao Pham Ngoc <thaopn@tech.est-rouge.com>
2022-08-10 15:09:12 +0700

    get credential for google api

---
+++ google/quickstart.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_token():
+    token_path = os.path.join(dir_path, "token.json")
+    credential_path = os.path.join(dir_path, "credentials.json")
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(credential_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds.token
+
+
+if __name__ == '__main__':
+    print(f"Access token:\n{get_token()}")
